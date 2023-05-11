1d4239858e10c176ad9cb91ea94ed586452ddb8e
nkk1026 <126162398+nkk1026@users.noreply.github.com>
2023-05-11 11:52:10 +0000

    create helpers and models folder, config.ts

---
+++ config.ts
@@ -0,0 +1,17 @@
+// exports.config = {
+//  host: "postgres://jzkunnhv:Fkg2q6wJ4xhDeTyb26z99b74VuPTirNT@tiny.db.elephantsql.com/jzkunnhv",
+//  port: 5432,
+//  user: "jzkunnhv",
+//  password: "Fkg2q6wJ4xhDeTyb26z99b74VuPTirNT",
+//  database: "jzkunnhv",
+//  connection_limit: 100
+// }
+
+exports.config = {
+  host: 'tiny.db.elephantsql.com', // database host
+  port: 5432,
+  user: 'jzkunnhv', //database username
+  password: 'Fkg2q6wJ4xhDeTyb26z99b74VuPTirNT', // password
+  database: 'jzkunnhv', //database nane
+  connection_limit: 100
+}
